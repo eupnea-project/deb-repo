d7a74e90e609cd59b0a6a615cedcd0abf1c77fa3
Apacelus <46828271+apacelus@users.noreply.github.com>
2022-12-02 14:21:15 +0100

    Pack proper packages

---
+++ pack-deb.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# create dirs
+mkdir -p eupnea-utils/DEBIAN
+mkdir -p eupnea-utils/usr/bin
+mkdir -p eupnea-utils/etc
+
+# Clone the postinstall repo
+git clone --depth=1 https://eupnea-linux/postinstall.git --branch=move-to-packages
+
+# move postinstall scripts into package
+mv postinstall/scripts eupnea-utils/usr/bin
+
+# move postinstall configs into package
+mv postinstall/config eupnea-utils/etc
+
+# copy debian control files into package
+cp control eupnea-utils/DEBIAN
+
+# create package
+dpkg-deb --build --root-owner-group eupnea-utils
