66ba0286886353b2be140118fe979d279d1be430
Apacelus <46828271+apacelus@users.noreply.github.com>
2022-12-02 14:33:23 +0100

    Fix postinstall-scripts configs dir

--- pack-deb.sh
+++ pack-deb.sh
@@ -5,7 +5,7 @@ set -e
 # create dirs
 mkdir -p eupnea-utils/DEBIAN
 mkdir -p eupnea-utils/usr/bin
-mkdir -p eupnea-utils/etc
+mkdir -p eupnea-utils/etc/eupnea
 
 # Clone the postinstall repo
 git clone --depth=1 https://github.com/eupnea-linux/postinstall-scripts.git --branch=move-to-packages
@@ -14,7 +14,7 @@ git clone --depth=1 https://github.com/eupnea-linux/postinstall-scripts.git --br
 mv postinstall-scripts/scripts eupnea-utils/usr/bin
 
 # move postinstall configs into package
-mv postinstall-scripts/config eupnea-utils/etc
+mv postinstall-scripts/configs eupnea-utils/etc/eupnea
 
 # copy debian control files into package
 cp control eupnea-utils/DEBIAN
