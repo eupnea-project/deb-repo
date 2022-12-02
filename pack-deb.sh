b3828b038883f46c48ddafd3334fca1fb0dd51fc
Apacelus <46828271+apacelus@users.noreply.github.com>
2022-12-02 14:24:59 +0100

    Fix github repo link

--- pack-deb.sh
+++ pack-deb.sh
@@ -6,7 +6,7 @@ mkdir -p eupnea-utils/usr/bin
 mkdir -p eupnea-utils/etc
 
 # Clone the postinstall repo
-git clone --depth=1 https://eupnea-linux/postinstall.git --branch=move-to-packages
+git clone --depth=1 https://github.com/eupnea-linux/postinstall.git --branch=move-to-packages
 
 # move postinstall scripts into package
 mv postinstall/scripts eupnea-utils/usr/bin
