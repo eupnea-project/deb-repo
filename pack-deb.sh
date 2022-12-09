5a3a229a4930e2042008bd5f0b9e4ca776947eb3
Apacelus <46828271+apacelus@users.noreply.github.com>
2022-12-09 09:02:01 +0100

    Update file hierarchy

--- pack-deb.sh
+++ pack-deb.sh
@@ -5,6 +5,7 @@ set -e
 # create dirs
 mkdir -p eupnea-utils/DEBIAN
 mkdir -p eupnea-utils/usr/bin
+mkdir -p eupnea-utils/usr/lib/eupnea
 mkdir -p eupnea-utils/etc/eupnea
 
 # Clone postinstall + audio repo
@@ -12,9 +13,11 @@ git clone --depth=1 https://github.com/eupnea-linux/postinstall-scripts.git --br
 git clone --depth=1 https://github.com/eupnea-linux/audio-scripts
 
 # move postinstall scripts into package
-mv postinstall-scripts/scripts/* eupnea-utils/usr/bin
-# move audio script into package
+mv postinstall-scripts/user-scripts/* eupnea-utils/usr/bin
 mv audio-scripts/setup-audio eupnea-utils/usr/bin
+# Move libs into package
+mv postinstall-scripts/system-scripts/* eupnea-utils/usr/lib/eupnea
+mv postinstall-scripts/functions.py eupnea-utils/usr/lib/eupnea
 
 # move postinstall configs into package
 mv postinstall-scripts/configs/* eupnea-utils/etc/eupnea
