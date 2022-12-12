bebdb40e6eebcc6b74cf71dbd843943c4765d0f1
Apacelus <46828271+apacelus@users.noreply.github.com>
2022-12-12 23:15:40 +0100

    Add configs to eupnea system package

--- pack-system.sh
+++ pack-system.sh
@@ -16,6 +16,9 @@ cp system-update/functions.py eupnea-system/tmp/eupnea-system-update
 cp system-update/eupnea_os_updates.py eupnea-system/tmp/eupnea-system-update
 cp system-update/depthboot_updates.py eupnea-system/tmp/eupnea-system-update
 
+# Copy configs
+cp system-update/configs/* eupnea-system/tmp/eupnea-system-update/configs/
+
 # copy debian control files into package
 cp system-control eupnea-system/DEBIAN/control
 
