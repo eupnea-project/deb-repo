37011d44aaa269d59da60214d413d2d6a8cf8059
Apacelus <46828271+apacelus@users.noreply.github.com>
2022-12-12 18:28:32 +0100

    Add postinstall hook + fix control files

--- pack-system.sh
+++ pack-system.sh
@@ -15,8 +15,12 @@ install -Dm 755 system-update/system-update.py eupnea-system/tmp/eupnea-system-u
 cp system-update/functions.py eupnea-system/tmp/eupnea-system-update
 cp system-update/eupnea_os_updates.py eupnea-system/tmp/eupnea-system-update
 cp system-update/depthboot_updates.py eupnea-system/tmp/eupnea-system-update
+
 # copy debian control files into package
 cp system-control eupnea-system/DEBIAN/control
+
+# Add postinst script to package
+cp postinst eupnea-system/DEBIAN/postinst
 
 # create package
 # by default dpkg-deb will use zstd compression. The deploy action will fail because the debian tool doesnt support zstd compression in packages.
