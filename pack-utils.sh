c7c7be6abba7037a6f5df9e3ff8e30fc0188f835
Apacelus <46828271+apacelus@users.noreply.github.com>
2023-01-14 18:24:44 +0100

    Remove cloud-utils dependency + remove postinstall step for utils package

--- pack-utils.sh
+++ pack-utils.sh
@@ -11,17 +11,14 @@ mkdir -p eupnea-utils/etc/systemd/system/
 
 # Clone postinstall + audio repo
 git clone --depth=1 https://github.com/eupnea-linux/postinstall-scripts.git
-git clone --depth=1 https://github.com/eupnea-linux/audio-scripts
-git clone --depth=1 https://github.com/eupnea-linux/systemd-services
+git clone --depth=1 https://github.com/eupnea-linux/audio-scripts.git
 
 # Copy scripts into package
 install -Dm 755 postinstall-scripts/user-scripts/* eupnea-utils/usr/bin
 install -Dm 755 audio-scripts/setup-audio eupnea-utils/usr/bin
 
 # Copy systemd services into package
-cp systemd-services/eupnea-update.service eupnea-utils/etc/systemd/system/
-cp systemd-services/eupnea-postinstall.service eupnea-utils/etc/systemd/system/
-cp systemd-services/eupnea-update.timer eupnea-utils/etc/systemd/system/
+cp postinstall-scripts/systemd-services/eupnea-postinstall.service eupnea-utils/etc/systemd/system/
 
 # Copy libs into package
 cp postinstall-scripts/system-scripts/* eupnea-utils/usr/lib/eupnea
@@ -33,8 +30,6 @@ cp -r audio-scripts/configs/* eupnea-utils/etc/eupnea
 
 # copy debian control files into package
 cp utils-control eupnea-utils/DEBIAN/control
-# Add postinst script to package
-install -Dm 755 utils-postinst eupnea-utils/DEBIAN/postinst
 
 # create package
 # by default dpkg-deb will use zstd compression. The deploy action will fail because the debian tool doesnt support zstd compression in packages.
