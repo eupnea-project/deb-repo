925b6a42a830c07ea875628fea2a77e8024596b5
Apacelus <46828271+apacelus@users.noreply.github.com>
2023-03-15 19:40:08 +0100

    Add and enable touchscreen fix service

--- pack-scripts/pack-utils.sh
+++ pack-scripts/pack-utils.sh
@@ -18,8 +18,7 @@ install -Dm 755 remote-eupnea-utils/user-scripts/* eupnea-utils/usr/bin
 install -Dm 755 audio-scripts/setup-audio eupnea-utils/usr/bin
 
 # Copy systemd services into package
-cp remote-eupnea-utils/configs/systemd-services/eupnea-postinstall.service eupnea-utils/etc/systemd/system/
-cp remote-eupnea-utils/configs/systemd-services/eupnea-update.service eupnea-utils/etc/systemd/system/
+cp remote-eupnea-utils/systemd-services/* eupnea-utils/etc/systemd/system
 
 # Copy libs into package
 cp remote-eupnea-utils/system-scripts/* eupnea-utils/usr/lib/eupnea
@@ -31,6 +30,8 @@ cp -r audio-scripts/configs/* eupnea-utils/etc/eupnea
 
 # copy debian control files into package
 cp control-files/utils-control eupnea-utils/DEBIAN/control
+# Add postinst script to package
+install -Dm 755 postinst-scripts/utils-postinst eupnea-utils/DEBIAN/postinst
 
 # create package
 # by default dpkg-deb will use zstd compression. The deploy action will fail because the debian tool doesnt support zstd compression in packages.
