26b39e8a74c2a1617ee15e88446e60cd0bc5504e
Apacelus <46828271+apacelus@users.noreply.github.com>
2023-12-07 09:51:06 +0100

    Add boards.json file from wtt repo + add bsd3 license for it

--- pack-scripts/pack-keyd.sh
+++ pack-scripts/pack-keyd.sh
@@ -5,6 +5,7 @@ set -e
 # clone repos
 git clone --depth=1 https://github.com/rvaiya/keyd.git keyd-remote
 git clone --depth=1 https://github.com/eupnea-project/eupnea-utils.git
+git clone --depth=1 https://github.com/weirdtreething/chromebook-linux-audio.git
 
 # make dir
 mkdir -p keyd/DEBIAN
@@ -25,11 +26,17 @@ cp configs/keyd.quirks keyd/usr/share/libinput
 # add keyboard configs
 cp -r eupnea-utils/configs/keyboard-layouts keyd/usr/share/eupnea/
 
+# add generations json
+cp weirdtreething/chromebook-linux-audio/boards.json keyd/usr/share/eupnea/board-generations.json
+
 # Add postinst script to package
 install -Dm 755 postinst-scripts/keyd-postinst keyd/DEBIAN/postinst
 
 # copy debian control file into package
 cp control-files/keyd-control keyd/DEBIAN/control
+
+# add license file
+cp licenses/keyd keyd/DEBIAN/copyright
 
 # create package
 # by default dpkg-deb will use zstd compression. The deploy action will fail because the debian tool doesnt support zstd compression in packages.
